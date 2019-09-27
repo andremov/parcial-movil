d049d17971411e9c21c410631dba6d600a5177e6
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-27 16:54:12 -0500

    log in activity view, main activity view

--- project/app/src/main/java/com/example/myapplication/MainActivity.java
+++ project/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // CUANDO QUIERE HACER LOG IN
+        ((Button)findViewById(R.id.login_button)).
+                setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intentToBecalled=new
+                                Intent(getApplicationContext(),
+                                MainActivity.class);
+                        intentToBecalled.putExtra("user_name",
+                                ((EditText)findViewById(
+                                        R.id.login_user_name)).getText().toString());
+                        intentToBecalled.putExtra("user_password",
+                                ((EditText)findViewById(
+                                        R.id.login_password)).getText().toString());
+                        startActivity(intentToBecalled);
+                    }
+                });
+
+        // CUANDO QUIERE HACER SIGN UP
+        ((Button)findViewById(R.id.signup_button)).
+                setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intentToBecalled=new
+                                Intent(getApplicationContext(),
+                                MainActivity.class);
+                        intentToBecalled.putExtra("user_name",
+                                ((EditText)findViewById(
+                                        R.id.login_user_name)).getText().toString());
+                        intentToBecalled.putExtra("user_password",
+                                ((EditText)findViewById(
+                                        R.id.login_password)).getText().toString());
+                        startActivity(intentToBecalled);
+                    }
+                });
     }
 }
