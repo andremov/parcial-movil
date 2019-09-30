315c7384ebd9519000cd160c48d6275b9ce863ef
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-29 19:02:20 -0500

    moving things

---
+++ project/app/src/main/java/com/example/myapplication/LaunchActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // CUANDO QUIERE HACER LOG IN
+        ((Button)findViewById(R.id.login_button)).
+                setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(LaunchActivity.this, LogInActivity.class));
+                    }
+                });
+
+        // CUANDO QUIERE HACER SIGN UP
+        ((Button)findViewById(R.id.signup_button)).
+                setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(LaunchActivity.this, SignUpActivity.class));
+                    }
+                });
+    }
+}
--- project/app/src/main/java/com/example/myapplication/MainActivity.java
+++ project/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // CUANDO QUIERE HACER LOG IN
-        ((Button)findViewById(R.id.login_button)).
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(MainActivity.this, LogInActivity.class));
-                    }
-                });
-
-        // CUANDO QUIERE HACER SIGN UP
-        ((Button)findViewById(R.id.signup_button)).
-                setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
-                    }
-                });
     }
 }
