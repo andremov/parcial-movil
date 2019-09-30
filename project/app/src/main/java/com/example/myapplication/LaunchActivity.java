50da24217bdfea9fe7c8cc40ebcbe4b0149ff368
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-29 23:48:50 -0500

    rename un poco de files

--- project/app/src/main/java/com/example/myapplication/LaunchActivity.java
+++ project/app/src/main/java/com/example/myapplication/LaunchActivity.java
@@ -6,14 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_menu_main);
 
         // CUANDO QUIERE HACER LOG IN
         ((Button)findViewById(R.id.login_button)).
